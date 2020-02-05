6446da9f012bf737d74cf1de296aeb58254c79f9
nwscala <nwscala@gmail.com>
2020-02-04 20:45:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions unless test?
+        set :session_secret, "secret"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session_item = session[:item]
+        "#{@session_item}"
+    end
 end
