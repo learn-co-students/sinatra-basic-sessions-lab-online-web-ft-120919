7bd88c8351f462d4303ff23315609444e51190b4
ricardo39985 <ricardo39985@gmail.com>
2020-02-06 10:01:23 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+   configure do
+    enable :sessions
+    set :session_secret, "killamanjaro"      
+   end 
+    get '/' do
+        # binding.pry
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        # binding.pry
+    end
 end
